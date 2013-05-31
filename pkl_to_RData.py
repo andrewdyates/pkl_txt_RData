1c179dfe16580dcdb8afdd8e630a74ef0fd02e2b
Andrew Yates <ayates@master.cl.osumc.edu>
2013-05-31 00:43:54 -0400

    use more sophesticated subprocess call to dispatch R job

--- pkl_to_RData.py
+++ pkl_to_RData.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python
-"""Wrapper to convert pkl to txt, then to RData in a single script."""
+"""Wrapper to convert pkl to txt, then to RData in a single script.
+
+USE:
+/usr/bin/time python $HOME/source_code/pkl_txt_RData/pkl_to_RData.py pkl_fname=$HOME/brca/GSE31448/GSE31448.SCAN.pkl.DCOR.values.pkl row_fname=$HOME/brca/GSE31448/normed/GSE31448.SCAN.tab col_fname=$HOME/brca/GSE31448/normed/GSE31448.SCAN.tab outdir=$HOME/brca/GSE31448/
+"""
 import sys,os
 import pkl_to_txt
 import subprocess
+from subprocess import Popen, PIPE, STDOUT
 
+R_TMP = open(os.path.join(os.path.dirname(__file__),"txt_to_RData.R.tmp")).read()
 DIR = os.path.dirname(__file__)
 
 def main(**argv):
   tab_fname = pkl_to_txt.main(**argv)
   R_fname = tab_fname + ".RData"
-  cmd = "R CMD BATCH %s/txt_to_RData.R %s" % (DIR, tab_fname)
-  print cmd
-  rcode = subprocess.call(cmd, shell=True)
-  print "RScript Return Code: ", rcode
+
+  print "Converting to RData binary object..."
   print "Expected R object file name: %s" % R_fname
+  r_script = R_TMP % {'fin':tab_fname, 'fout':R_fname}
+  print r_script
+  p = Popen(["R", "--vanilla", "--slave"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+  print p.communicate(input=r_script)
+  p.stdin.close()
   return R_fname
+  
 
 if __name__ == "__main__":
   argv = dict([s.split('=') for s in sys.argv[1:]])
